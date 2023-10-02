f617f3389c939ed56148a02958d8a4662f798165
C-jimenez21 <cristianjjc21@gmail.com>
2023-10-01 20:38:46 -0500

    feat: :sparkles: Create queries and endpoints for the Reserves collection

---
+++ api/controllers/v3/LoansAndReserves.js
@@ -0,0 +1,303 @@
+import { ObjectId } from "mongodb";
+import genCollection from "../../helpers/fastConnect.js"
+//COLECCION RESERVES
+
+export const getDataReserveV1 = async (req, res) => {
+    const coleccion = await genCollection("Reserves");
+    let result = await coleccion.aggregate([
+        {
+            $lookup: {
+                from: "User",
+                localField: "user",
+                foreignField: "email",
+                as: "User"
+            }
+        },
+        {
+            $unwind: "$User"
+        },
+        {
+            $lookup: {
+                from: "Product",
+                localField: "Product",
+                foreignField: "serial",
+                as: "Product"
+            }
+        },
+        {
+            $unwind: "$Product"
+        },
+        {
+            $project: {
+                user: 0,
+                "User._id": 0,
+                "Product._id": 0
+            }
+        }
+    ]).toArray();
+    res.send(result).status(200)
+}
+
+export const getDataReserveById = async (req, res) => {
+    try {
+        const coleccion = await genCollection("Reserves");
+        let result = await coleccion.aggregate([
+            { $match: { _id: new ObjectId(req.params.id) } },
+            {
+                $lookup: {
+                    from: "User",
+                    localField: "user",
+                    foreignField: "email",
+                    as: "User"
+                }
+            },
+            {
+                $unwind: "$User"
+            },
+            {
+                $lookup: {
+                    from: "Product",
+                    localField: "Product",
+                    foreignField: "serial",
+                    as: "Product"
+                }
+            },
+            {
+                $unwind: "$Product"
+            },
+            {
+                $project: {
+                    user: 0,
+
+                    "User._id": 0,
+                    "Product._id": 0
+                }
+            }]).toArray();
+        (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Reserves not found' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const getDataReserveByDate = async (req, res) => {
+    try {
+        const fechaFormat = new Date(req.body.date);
+        console.log(fechaFormat);
+        console.log(new Date(req.body.date));
+
+        const coleccion = await genCollection("Reserves");
+        let result = await coleccion.aggregate([
+            { $match: { reservedDate: fechaFormat } },
+            {
+                $lookup: {
+                    from: "User",
+                    localField: "user",
+                    foreignField: "email",
+                    as: "User"
+                }
+            },
+            {
+                $unwind: "$User"
+            },
+            {
+                $lookup: {
+                    from: "Product",
+                    localField: "Product",
+                    foreignField: "serial",
+                    as: "Product"
+                }
+            },
+            {
+                $unwind: "$Product"
+            },
+            {
+                $project: {
+                    user: 0,
+
+                    "User._id": 0,
+                    "Product._id": 0
+                }
+            }]).toArray();
+        (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Reserve not found' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const getDataReserveByDiferentParam = async (req, res) => {
+    try {
+        const { field, value } = req.body
+        const coleccion = await genCollection("Reserves");
+        console.log(field, value);
+        if (field == "state") {
+            let result = await coleccion.aggregate([
+                { $match: { state: value } },
+                {
+                    $lookup: {
+                        from: "User",
+                        localField: "user",
+                        foreignField: "email",
+                        as: "User"
+                    }
+                },
+                {
+                    $unwind: "$User"
+                },
+                {
+                    $lookup: {
+                        from: "Product",
+                        localField: "Product",
+                        foreignField: "serial",
+                        as: "Product"
+                    }
+                },
+                {
+                    $unwind: "$Product"
+                },
+                {
+                    $project: {
+                        user: 0,
+
+                        "User._id": 0,
+                        "Product._id": 0
+                    }
+                }]).toArray();
+            (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Reserve not found' })
+            return result;
+        } else if (field == "user") {
+            let result = await coleccion.aggregate([
+                { $match: { user: value } },
+                {
+                    $lookup: {
+                        from: "User",
+                        localField: "user",
+                        foreignField: "email",
+                        as: "User"
+                    }
+                },
+                {
+                    $unwind: "$User"
+                },
+                {
+                    $lookup: {
+                        from: "Product",
+                        localField: "Product",
+                        foreignField: "serial",
+                        as: "Product"
+                    }
+                },
+                {
+                    $unwind: "$Product"
+                },
+                {
+                    $project: {
+                        user: 0,
+
+                        "User._id": 0,
+                        "Product._id": 0
+                    }
+                }]).toArray();
+            (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Reserve not found' })
+            return result
+        }
+        return result
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const postReserve = async (req, res) => {
+    try {
+        //Validar la informacion
+        const { user, product, reservedDate, state } = req.data
+
+        //Revisar si este usuario ya se encuentra en la base de datos
+        const UserCol = await genCollection("User");
+        const isMatchA = await UserCol.findOne({ email: user });
+        if (!isMatchA) { return res.status(404).json({ message: "User not found" }); }
+
+        const ProductCol = await genCollection("Product");
+        const isMatch = await ProductCol.findOne({ serial: product });
+        if (!isMatch) { return res.status(404).json({ message: "Product not found" }); }
+
+        if (isMatch.stock == 0) return res.status(404).json({ message: 'There is not stock for this product.' })
+
+        //Realizar el registro en la base de datos
+        const ReseveObj = {
+            user,
+            Product: product,
+            reservedDate: new Date(reservedDate),
+            state
+        }
+        console.log(ReseveObj);
+        const Reserve = await genCollection("Reserves");
+        const newReserve = await Reserve.insertOne(ReseveObj)
+        console.log(newReserve);
+        let result
+        (newReserve.acknowledged) ? result = res.status(201).json({ message: 'Successfully created reserve' }) : result = res.status(404).json({ message: 'Reserve could not be registered' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const deleteReserveById = async (req, res) => {
+    try {
+      const coleccion = await genCollection("Reserves");
+      let result = await coleccion.deleteOne({_id: new ObjectId(req.params.id)})
+      console.log(result);
+      (result.acknowledged && result.deletedCount !== 0) ? result = res.send({message: 'Reserve successfully removed'}).status(204) : result = res.status(404).json({ message: 'Reserve not found' })
+      return result;
+  } catch (error) {
+      res.status(404).json({ message: 'Something went wrong' })
+      console.log(error);
+    }
+}
+
+
+
+//COLECCION LOANS
+
+
+export const getDataLoanV1 = async (req, res) => {
+    const coleccion = await genCollection("Loans");
+    let result = await coleccion.aggregate([
+        {
+            $lookup: {
+                from: "User",
+                localField: "user",
+                foreignField: "email",
+                as: "User"
+            }
+        },
+        {
+            $unwind: "$User"
+        },
+        {
+            $lookup: {
+                from: "Product",
+                localField: "Product",
+                foreignField: "serial",
+                as: "Product"
+            }
+        },
+        {
+            $unwind: "$Product"
+        },
+        {
+            $project: {
+                user: 0,
+
+                "User._id": 0,
+                "Product._id": 0
+            }
+        }
+    ]).toArray();
+    res.send(result).status(200)
+}
