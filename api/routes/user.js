21e0e24b49a74f801d8d0b6e8565a8b6f7126caf
C-jimenez21 <cristianjjc21@gmail.com>
2023-10-01 20:40:22 -0500

    feat: :sparkles: Validations for entering reservations are included

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,10 +3,10 @@ import { Router } from "express";
 import { getDataUserV1, getDataReserveV1, getDataLoanV1 } from "../controllers/v1/UserV1.js";
 import { validateToken } from "../middlewares/Token.js";
 const appUser = Router();
+
+
 appUser.get('/profile', validateToken, getDataUserV1);
 
-appUser.get('/reserves', getDataReserveV1);
-appUser.get('/loans', getDataLoanV1);
 
 
 export default appUser  
