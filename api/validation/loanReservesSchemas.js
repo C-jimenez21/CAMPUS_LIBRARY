8db0548a3c4d8621dca0a0fe1964b58e5b0adda2
C-jimenez21 <cristianjjc21@gmail.com>
2023-10-01 21:50:14 -0500

    feat: :sparkles: Validations for method POST in Loans collection

--- api/validation/loanReservesSchemas.js
+++ api/validation/loanReservesSchemas.js
@@ -22,5 +22,27 @@ export const reserveSchema = z.object({
         invalid_type_error: "reservedDate must be a string",
     }),
     state: z
-    .enum(['aprobada', 'rechazada', 'completada'])
+    .enum(['aprobada', 'rechazada', 'completada', 'pendiente']).optional()
+});
+
+
+export const loanSchema = z.object({
+    user: z.string({
+        required_error: "user is required",
+        invalid_type_error: "user must be a string",
+    }),
+    product: z.string({
+        required_error: "product is required",
+        invalid_type_error: "Product must be a string",
+    }),
+    beguinDate: z.string({
+        required_error: "beguinDate is required",
+        invalid_type_error: "beguinDate must be a string",
+    }),
+    endDate: z.string({
+        required_error: "endDate is required",
+        invalid_type_error: "endDate must be a string",
+    }),
+    state: z
+    .enum(['aprobada','rechazada', 'pendiente', 'completada', 'vencida']).optional()
 });
