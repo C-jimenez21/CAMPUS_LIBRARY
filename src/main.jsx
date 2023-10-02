75ff2e95db32a6f71a5680ff4025f8d43174dd11
C-jimenez21 <cristianjjc21@gmail.com>
2023-10-01 23:57:26 -0500

    feat: :sparkles: Test components are made to test the endpoints

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import BookList from './components/BookList.jsx'
+import DynamicTable from './components/BookList.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BookList/>
+     <DynamicTable></DynamicTable>
   </React.StrictMode>,
 )
