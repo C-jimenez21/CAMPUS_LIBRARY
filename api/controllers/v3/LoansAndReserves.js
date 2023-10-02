0a92635dfdec8d87d1aebc76eaee4d9f35fe5e03
C-jimenez21 <cristianjjc21@gmail.com>
2023-10-01 21:49:37 -0500

    feat: :sparkles: Create queries and endpoints for the Loans collection

--- api/controllers/v3/LoansAndReserves.js
+++ api/controllers/v3/LoansAndReserves.js
@@ -33,6 +33,9 @@ export const getDataReserveV1 = async (req, res) => {
                 "User._id": 0,
                 "Product._id": 0
             }
+        },
+        {
+            $sort: { "reservedDate": -1 }
         }
     ]).toArray();
     res.send(result).status(200)
@@ -165,7 +168,9 @@ export const getDataReserveByDiferentParam = async (req, res) => {
                         "User._id": 0,
                         "Product._id": 0
                     }
-                }]).toArray();
+                },
+                { $sort: { "reservedDate": -1 } }
+            ]).toArray();
             (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Reserve not found' })
             return result;
         } else if (field == "user") {
@@ -200,7 +205,9 @@ export const getDataReserveByDiferentParam = async (req, res) => {
                         "User._id": 0,
                         "Product._id": 0
                     }
-                }]).toArray();
+                },
+                { $sort: { "reservedDate": -1 } }
+            ]).toArray();
             (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Reserve not found' })
             return result
         }
@@ -249,14 +256,14 @@ export const postReserve = async (req, res) => {
 
 export const deleteReserveById = async (req, res) => {
     try {
-      const coleccion = await genCollection("Reserves");
-      let result = await coleccion.deleteOne({_id: new ObjectId(req.params.id)})
-      console.log(result);
-      (result.acknowledged && result.deletedCount !== 0) ? result = res.send({message: 'Reserve successfully removed'}).status(204) : result = res.status(404).json({ message: 'Reserve not found' })
-      return result;
-  } catch (error) {
-      res.status(404).json({ message: 'Something went wrong' })
-      console.log(error);
+        const coleccion = await genCollection("Reserves");
+        let result = await coleccion.deleteOne({ _id: new ObjectId(req.params.id) })
+        console.log(result);
+        (result.acknowledged && result.deletedCount !== 0) ? result = res.send({ message: 'Reserve successfully removed' }).status(204) : result = res.status(404).json({ message: 'Reserve not found' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
     }
 }
 
@@ -293,11 +300,241 @@ export const getDataLoanV1 = async (req, res) => {
         {
             $project: {
                 user: 0,
-
                 "User._id": 0,
                 "Product._id": 0
             }
-        }
+        },
+        { $sort:{"beguinDate": -1}}
     ]).toArray();
-    res.send(result).status(200)
+    (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Loans not found' })
+    return result
+}
+
+
+export const getDataLoanById = async (req, res) => {
+    try {
+        const coleccion = await genCollection("Loans");
+        let result = await coleccion.aggregate([
+            { $match: { _id: new ObjectId(req.params.id) } },
+            
+            {
+                $lookup: {
+                    from: "User",
+                    localField: "user",
+                    foreignField: "email",
+                    as: "User"
+                }
+            },
+            {
+                $unwind: "$User"
+            },
+            {
+                $lookup: {
+                    from: "Product",
+                    localField: "Product",
+                    foreignField: "serial",
+                    as: "Product"
+                }
+            },
+            {
+                $unwind: "$Product"
+            },
+            {
+                $project: {
+                    user: 0,
+                    "User._id": 0,
+                    "Product._id": 0
+                }
+            }
+        ]).toArray();
+        (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Loans not found' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const getDataLoanByDate = async (req, res) => {
+    try {
+        const fechaFormat = new Date(req.body.date);
+        console.log(fechaFormat);
+        console.log(new Date(req.body.date));
+
+        const coleccion = await genCollection("Loans");
+        let result = await coleccion.aggregate([
+            { $match: { beguinDate: fechaFormat } },
+            {
+                $lookup: {
+                    from: "User",
+                    localField: "user",
+                    foreignField: "email",
+                    as: "User"
+                }
+            },
+            {
+                $unwind: "$User"
+            },
+            {
+                $lookup: {
+                    from: "Product",
+                    localField: "Product",
+                    foreignField: "serial",
+                    as: "Product"
+                }
+            },
+            {
+                $unwind: "$Product"
+            },
+            {
+                $project: {
+                    user: 0,
+
+                    "User._id": 0,
+                    "Product._id": 0
+                }
+            },
+            { $sort:{"beguinDate": -1}}
+        ]).toArray();
+        (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Loan not found' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const getDataLoanByDiferentParam = async (req, res) => {
+    try {
+        const { field, value } = req.body
+        const coleccion = await genCollection("Loans");
+        console.log(field, value);
+        if (field == "state") {
+            let result = await coleccion.aggregate([
+                { $match: { state: value } },
+                {
+                    $lookup: {
+                        from: "User",
+                        localField: "user",
+                        foreignField: "email",
+                        as: "User"
+                    }
+                },
+                {
+                    $unwind: "$User"
+                },
+                {
+                    $lookup: {
+                        from: "Product",
+                        localField: "Product",
+                        foreignField: "serial",
+                        as: "Product"
+                    }
+                },
+                {
+                    $unwind: "$Product"
+                },
+                {
+                    $project: {
+                        user: 0,
+
+                        "User._id": 0,
+                        "Product._id": 0
+                    }
+                },
+                { $sort:{"beguinDate": -1}}]).toArray();
+            (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Loan not found' })
+            return result;
+        } else if (field == "user") {
+            let result = await coleccion.aggregate([
+                { $match: { user: value } },
+                {
+                    $lookup: {
+                        from: "User",
+                        localField: "user",
+                        foreignField: "email",
+                        as: "User"
+                    }
+                },
+                {
+                    $unwind: "$User"
+                },
+                {
+                    $lookup: {
+                        from: "Product",
+                        localField: "Product",
+                        foreignField: "serial",
+                        as: "Product"
+                    }
+                },
+                {
+                    $unwind: "$Product"
+                },
+                {
+                    $project: {
+                        user: 0,
+
+                        "User._id": 0,
+                        "Product._id": 0
+                    }
+                },
+                { $sort:{"beguinDate": -1}}]).toArray();
+            (result.length > 0) ? result = res.send(result).status(200) : result = res.status(404).json({ message: 'Loan not found' })
+            return result
+        }
+        return result
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const postLoan = async (req, res) => {
+    try {
+        //Validar la informacion
+        const { user, product, beguinDate, endDate, state= "pediente" } = req.data
+
+        //Revisar si este usuario ya se encuentra en la base de datos
+        const UserCol = await genCollection("User");
+        const isMatchA = await UserCol.findOne({ email: user });
+        if (!isMatchA) { return res.status(404).json({ message: "User not found" }); }
+
+        const ProductCol = await genCollection("Product");
+        const isMatch = await ProductCol.findOne({ serial: product });
+        if (!isMatch) { return res.status(404).json({ message: "Product not found" }); }
+
+        if (isMatch.stock == 0) return res.status(404).json({ message: 'There is not stock for this product.' })
+
+        //Realizar el registro en la base de datos
+        const ReseveObj = {
+            user,
+            Product: product,
+            beguinDate: new Date(beguinDate),
+            endDate: new Date(endDate),
+            state 
+        }
+        console.log(ReseveObj);
+        const Loan = await genCollection("Loans");
+        const newLoan = await Loan.insertOne(ReseveObj)
+        console.log(newLoan);
+        let result
+        (newLoan.acknowledged) ? result = res.status(201).json({ message: 'Successfully created Loan' }) : result = res.status(404).json({ message: 'Loan could not be registered' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
+}
+
+export const deleteLoanById = async (req, res) => {
+    try {
+        const coleccion = await genCollection("Loans");
+        let result = await coleccion.deleteOne({ _id: new ObjectId(req.params.id) })
+        console.log(result);
+        (result.acknowledged && result.deletedCount !== 0) ? result = res.send({ message: 'Loan successfully removed' }).status(204) : result = res.status(404).json({ message: 'Reserve not found' })
+        return result;
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+        console.log(error);
+    }
 }
--- api/routes/LoansAndReserves.js
+++ api/routes/LoansAndReserves.js
@@ -1,20 +1,21 @@
 //user routers
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validatorSchema.js";
-//import { getDataUserV1, getDataProductV1, getDataReserveV1, getDataLoanV1 } from "../controllers/v1/UserV1.js";
-import { getDataLoanV1, getDataReserveV1, getDataReserveById, getDataReserveByDate, getDataReserveByDiferentParam, postReserve, deleteReserveById} from "../controllers/v3/LoansAndReserves.js";
-import { reserveSchema } from "../validation/loanReservesSchemas.js";
+import { getDataLoanV1, getDataReserveV1, getDataReserveById, getDataReserveByDate, getDataReserveByDiferentParam, postReserve, deleteReserveById, getDataLoanById, getDataLoanByDiferentParam, getDataLoanByDate, postLoan, deleteLoanById} from "../controllers/v3/LoansAndReserves.js";
+import { reserveSchema, loanSchema } from "../validation/loanReservesSchemas.js";
 
 const appLoans = Router();
 const appReserves = Router();
 
-
+//LOANS
 appLoans.get('/', getDataLoanV1);
-//appLoans.get('/:id', getDataProductById);
-//appLoans.post('/', validateSchema(productSchema), postProduct);
-//appLoans.delete('/:id', deleteProductById);
+appLoans.get('/:id', getDataLoanById);
+appReserves.get('/search', getDataLoanByDiferentParam);
+appReserves.get('/date', getDataLoanByDate);
+appLoans.post('/', validateSchema(loanSchema), postLoan);
+appLoans.delete('/:id', deleteLoanById);
 
-
+//RESERVES
 appReserves.get('/', getDataReserveV1);
 appReserves.get('/search', getDataReserveByDiferentParam);
 appReserves.get('/date', getDataReserveByDate);
