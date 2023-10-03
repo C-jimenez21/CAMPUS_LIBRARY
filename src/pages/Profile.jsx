93ae563d183a8fa992251e1992c33f2685021fb6
C-jimenez21 <cristianjjc21@gmail.com>
2023-10-03 16:36:11 -0500

    refactor: :recycle: Code refactoring to add a path to the logout page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -112,7 +112,7 @@ function Profile() {
 
 
   const { user, logOut } = useAuth()
-  console.log(user);
+
 
   return (
     <>
@@ -205,28 +205,11 @@ function Profile() {
 
 
       
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+   
+
+      
       <div>
-      {['left', 'right', 'top', 'bottom'].map((anchor) => (
+      {['left'].map((anchor) => (
         <React.Fragment key={anchor}>
           <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
           <Drawer
@@ -239,6 +222,10 @@ function Profile() {
         </React.Fragment>
       ))}
     </div>
+
+
+
+
       {/* <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
       <BottomNavigationAction
         label="Recents"
